fd00b2a55a1ee66fdf71a8ea6ceb1f3c393fc89d
mrossi <martin.rossi@lemondata.com.ar>
2018-09-14 11:43:51 -0300

    se crea nueva seccion

---
+++ Seccion04/app/src/main/java/adapters/MyAdapter.java
@@ -0,0 +1,85 @@
+package adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.seccion02.seccion04.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Pelicula;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+	private List<Pelicula> peliculas = new ArrayList<Pelicula>();
+	private int layout;
+	private OnItemClickListener onItemClickListener;
+	// Este contexto es necesario para la libreria Picasso
+	private Context context;
+
+	public MyAdapter(List<Pelicula> peliculas, int layout, OnItemClickListener onItemClickListener) {
+		this.peliculas = peliculas;
+		this.layout = layout;
+		this.onItemClickListener = onItemClickListener;
+	}
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		context = parent.getContext();
+		View view = LayoutInflater.from(context).inflate(layout, parent, false);
+		ViewHolder viewHolder = new ViewHolder(view);
+		return viewHolder;
+	}
+
+	// metodo que se ejecuta cuando se crea el RecyclerView. Realiza el volcado de datos
+	@Override
+	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+		holder.bind(peliculas.get(position), onItemClickListener);
+	}
+
+	@Override
+	public int getItemCount() {
+		return peliculas.size();
+	}
+
+	public static class ViewHolder extends RecyclerView.ViewHolder {
+		public TextView textViewDescripcion;
+		public ImageView imageView;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+			this.textViewDescripcion = itemView.findViewById(R.id.textViewDescripcion);
+			this.imageView = itemView.findViewById(R.id.imageView);
+		}
+
+		public void bind(final Pelicula pelicula, final OnItemClickListener onItemClickListener) {
+
+			// -- Se se setean todos los elementos del layout
+			textViewDescripcion.setText(pelicula.getNombre());
+			Picasso.get().load(pelicula.getImagen()).fit().into(imageView);
+			// imageView.setImageResource(pelicula.getImagen());
+			// ----------------------------------------------
+
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// se le pasa el nombre y la posicion
+					onItemClickListener.onItemClick(pelicula, getAdapterPosition());
+				}
+			});
+		}
+	}
+
+	public interface OnItemClickListener {
+		void onItemClick(Pelicula pelicula, int position);
+	}
+}
--- Seccion04/app/src/main/java/com/seccion02/seccion04/MainActivity.java
+++ Seccion04/app/src/main/java/com/seccion02/seccion04/MainActivity.java
@@ -1,13 +1,104 @@
 package com.seccion02.seccion04;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapters.MyAdapter;
+import models.Pelicula;
 
 public class MainActivity extends AppCompatActivity {
+	private List<Pelicula> peliculas;
+	private RecyclerView recyclerView;
+	private RecyclerView.Adapter adapter;
+	private RecyclerView.LayoutManager layoutManager;
+	private int contador = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		setearLista();
+		recyclerView = findViewById(R.id.recyclerView);
+
+
+		// ----------- Tipos de Layout
+		// Layout lineal
+		layoutManager = new LinearLayoutManager(this);
+		// Layout de grilla
+		// layoutManager = new GridLayoutManager(this, 2);
+		// Esto es como un grid donde tienen diferentes tamaños, como cuando ponene imagenes como en
+		// mozaicos que unas son mas grandes que otras, medias desordenadas
+		// layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+		// -----------------------------------------------
+		adapter = new MyAdapter(peliculas, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
+			@Override
+			public void onItemClick(Pelicula pelicula, int position) {
+				eliminarItem(position);
+				// Toast.makeText(MainActivity.this, nombre + " eliminado", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setAdapter(adapter);
+		// si sabemos que este layout no va a ser mas grande, como por ejemplo, el nombre no va a ser muy grande, etc.
+		// Seteando esta propiedad en true mejora la performance
+		recyclerView.setHasFixedSize(true);
+
+		// Le se teamos una animacion, en este caso, la que viene por defecto. Pero se puede crear una personalizada
+		recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.my_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean resultado;
+
+		switch (item.getItemId()) {
+			case R.id.itemMenuAgregar:
+				agregarItem(0);
+				resultado = true;
+				break;
+			default:
+				resultado = super.onOptionsItemSelected(item);
+				break;
+		}
+		return resultado;
+	}
+
+	private void agregarItem(int posicion) {
+		peliculas.add(posicion, new Pelicula("Nuevo Nombre " + contador++, R.drawable.imagen_default));
+		adapter.notifyItemInserted(posicion);
+		layoutManager.scrollToPosition(posicion);
+	}
+
+	private void eliminarItem(int posicion) {
+		peliculas.remove(posicion);
+		adapter.notifyItemRemoved(posicion);
+	}
+
+	private void setearLista() {
+		peliculas = new ArrayList<Pelicula>();
+		peliculas.add(new Pelicula("Fury", R.drawable.fury));
+		peliculas.add(new Pelicula("Batman", R.drawable.joker));
+		peliculas.add(new Pelicula("Wolf of Wall Street", R.drawable.wolf_of_wall_street));
+		peliculas.add(new Pelicula("Hot Line Maiame", R.drawable.maiame));
+		peliculas.add(new Pelicula("Oblivion", R.drawable.oblivion));
+		peliculas.add(new Pelicula("The Good the Bad and the Ugly", R.drawable.the_good_the_bad_and_the_ugly));
 	}
 }
