b02de75783cc2b0cdd5f7eaf35d5c2d4c5068cac
martin <martinrossi9009@gmail.com>
2019-10-30 13:11:42 -0300

    a

---
+++ Seccion09_fragment/app/src/main/java/com/example/seccion09_fragment/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.seccion09_fragment.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.seccion09_fragment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
