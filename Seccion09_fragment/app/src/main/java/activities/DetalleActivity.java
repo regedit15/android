9aa23591555911dd27f1e9ada94a55d6dad4d19d
martin <martinrossi9009@gmail.com>
2019-11-03 11:14:29 -0300

    i

--- Seccion09_fragment/app/src/main/java/activities/DetalleActivity.java
+++ Seccion09_fragment/app/src/main/java/activities/DetalleActivity.java
@@ -1,4 +1,4 @@
-package actividades;
+package activities;
 
 import android.os.Bundle;
 
--- Seccion09_fragment/app/src/main/java/activities/MainActivity.java
+++ Seccion09_fragment/app/src/main/java/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package actividades;
+package activities;
 
 import android.content.Intent;
 import android.os.Bundle;
