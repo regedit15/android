6decb115528d8508de500930e7aa5bebf83cc1bd
martin <martinrossi9009@gmail.com>
2019-11-11 11:28:12 -0300

    u

--- Seccion10_tabs/app/src/main/java/com/example/seccion10_tabs/MainActivity.java
+++ Seccion10_tabs/app/src/main/java/com/example/seccion10_tabs/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.seccion10_tabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Toolbar myToolbar = findViewById(R.id.my_toolbar);
+		//Esto es para convertir el toolbar en un action bar. Por lo que mostrara el titulo
+		setSupportActionBar(myToolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
