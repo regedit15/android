de26fabe7c528a1d19bea883033c3af1f868d519
martin <martinrossi9009@gmail.com>
2019-11-19 11:16:34 -0300

    r

---
+++ BotoneraForgottera/app/src/main/java/martin/botoneraforgottera/MainActivity.java
@@ -0,0 +1,14 @@
+package martin.botoneraforgottera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
