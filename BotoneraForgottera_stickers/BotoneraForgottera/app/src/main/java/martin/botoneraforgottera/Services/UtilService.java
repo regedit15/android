0d3fb2812ddbb450f80c5810bf6e89968cfd439b
martin <martinrossi9009@gmail.com>
2019-11-25 20:00:35 -0300

    a

--- BotoneraForgottera_stickers/BotoneraForgottera/app/src/main/java/martin/botoneraforgottera/Services/UtilService.java
+++ BotoneraForgottera_stickers/BotoneraForgottera/app/src/main/java/martin/botoneraforgottera/Services/UtilService.java
@@ -12,12 +12,12 @@ public class UtilService {
 	public List<Audio> getAudios() {
 		return new ArrayList<Audio>() {{
 			add(new Audio(R.raw.uber1, "Te voy a tocar la jalea", "Descripción 1", new ArrayList<Tag>() {{
-				add(new Tag("uber"));
-				add(new Tag("jalea"));
+				add(new Tag("#uber"));
+				add(new Tag("#jalea"));
 			}}));
 			add(new Audio(R.raw.luacha1, "Luacha 1", "Descripción 22", new ArrayList<Tag>() {{
-				add(new Tag("luacha"));
-				add(new Tag("grito"));
+				add(new Tag("#luacha"));
+				add(new Tag("#grito"));
 			}}));
 		}};
 	}
