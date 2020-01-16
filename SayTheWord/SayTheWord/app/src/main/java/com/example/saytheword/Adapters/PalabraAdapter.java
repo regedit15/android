1ed3f6b6f23fe055b39ce5a19f201d1e7f896b50
martin <martinrossi9009@gmail.com>
2020-01-16 09:45:14 -0300

    y

---
+++ SayTheWord/SayTheWord/app/src/main/java/com/example/saytheword/Adapters/PalabraAdapter.java
@@ -0,0 +1,182 @@
+package com.example.saytheword.Adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.saytheword.Activities.MainActivity;
+import com.example.saytheword.Models.Palabra;
+import com.example.saytheword.R;
+import com.example.saytheword.Services.RealmService;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import static com.example.saytheword.Services.UtilService.LISTADO_TIPO_TRADUCCION_ESPANIOL_INGLES;
+import static com.example.saytheword.Services.UtilService.LISTADO_TIPO_TRADUCCION_INGLES_ESPANIOL;
+
+public class PalabraAdapter extends RecyclerView.Adapter<PalabraAdapter.ViewHolder> {
+
+	private List<Palabra> lista;
+	private int layout;
+	private String TIPO_TRADUCCION;
+	private RealmService realmService = new RealmService();
+	private Activity activity;
+
+	public PalabraAdapter(List<Palabra> lista, int layout, String TIPO_TRADUCCION, Activity activity) {
+		this.lista = lista;
+		this.layout = layout;
+		this.TIPO_TRADUCCION = TIPO_TRADUCCION;
+		this.activity = activity;
+	}
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		View view = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+		ViewHolder viewHolder = new ViewHolder(view);
+		return viewHolder;
+	}
+
+	// metodo que se ejecuta cuando se crea el RecyclerView. Realiza el volcado de datos
+	@Override
+	public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+		holder.bind(lista.get(position));
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+
+		public TextView tvPalabraIzquierda;
+		public TextView tvPalabraDerechaArriba;
+		public TextView tvPalabraDerechaAbajo;
+		public ConstraintLayout lyRespuesta;
+		public MaterialButton mbMostrarRespuesta;
+		public MaterialButton btPalabraProblematicaListado;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+
+			this.tvPalabraIzquierda = itemView.findViewById(R.id.tvPalabraIzquierda);
+			this.tvPalabraDerechaArriba = itemView.findViewById(R.id.tvPalabraDerechaArriba);
+			this.tvPalabraDerechaAbajo = itemView.findViewById(R.id.tvPalabraDerechaAbajo);
+			this.lyRespuesta = itemView.findViewById(R.id.lyRespuesta);
+			this.mbMostrarRespuesta = itemView.findViewById(R.id.btMostrarRespuesta);
+			this.btPalabraProblematicaListado = itemView.findViewById(R.id.btPalabraProblematicaListado);
+		}
+
+		public void bind(final Palabra palabra) {
+
+			switch (TIPO_TRADUCCION) {
+				case LISTADO_TIPO_TRADUCCION_ESPANIOL_INGLES:
+					tvPalabraIzquierda.setText(palabra.getPalabraEsp());
+					tvPalabraDerechaArriba.setText(palabra.getPalabraIng());
+					break;
+				case LISTADO_TIPO_TRADUCCION_INGLES_ESPANIOL:
+					tvPalabraIzquierda.setText(palabra.getPalabraIng());
+					tvPalabraDerechaArriba.setText(palabra.getPalabraEsp());
+					break;
+			}
+			tvPalabraDerechaAbajo.setText(palabra.getPronunciacion());
+
+
+			setear(palabra.isMostrarRespuesta());
+			setearColorPalabraProblematica(palabra);
+
+			mbMostrarRespuesta.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					realmService.cambiarMostrarRespuestaPalabra(palabra);
+					setear(palabra.isMostrarRespuesta());
+				}
+			});
+
+			btPalabraProblematicaListado.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					realmService.cambiarPalabraProblematicaPalabra(palabra);
+					setearColorPalabraProblematica(palabra);
+					notifyDataSetChanged();
+
+					((MainActivity) activity).getSupportActionBar().setTitle(new StringBuilder(((MainActivity) activity).navigationView.getMenu().getItem(0).getTitle().toString()).append(" (").append(lista.size()).append(")").toString());
+				}
+			});
+		}
+
+		private void setear(boolean mostrarRespuesta) {
+			if (mostrarRespuesta) {
+				lyRespuesta.setVisibility(View.VISIBLE);
+				mbMostrarRespuesta.setIconResource(R.drawable.ic_eye_open);
+			} else {
+				lyRespuesta.setVisibility(View.INVISIBLE);
+				mbMostrarRespuesta.setIconResource(R.drawable.ic_eye_closed);
+			}
+		}
+
+		private void setearColorPalabraProblematica(Palabra palabra) {
+			if (palabra.isPalabraProblematica()) {
+				btPalabraProblematicaListado.setIconResource(R.drawable.ic_angry);
+				btPalabraProblematicaListado.setBackgroundTintList(activity.getResources().getColorStateList(R.color.colorPalabraProblematica));
+			} else {
+				btPalabraProblematicaListado.setIconResource(R.drawable.ic_smile);
+				btPalabraProblematicaListado.setBackgroundTintList(activity.getResources().getColorStateList(R.color.colorPalabraBuena));
+			}
+		}
+
+	}
+
+	public void ocultarTodo() {
+		cambiarTodo(false);
+	}
+
+	public void verTodo() {
+		cambiarTodo(true);
+	}
+
+	private void cambiarTodo(final boolean valor) {
+		realmService.cambiarMostrarRespuestasPalabras(lista, valor);
+		notifyDataSetChanged();
+	}
+
+	public void cambiarAInglesEspaniol() {
+		cambiarTipoListado(LISTADO_TIPO_TRADUCCION_INGLES_ESPANIOL);
+	}
+
+	public void cambiarAEspaniolIngles() {
+		cambiarTipoListado(LISTADO_TIPO_TRADUCCION_ESPANIOL_INGLES);
+	}
+
+	private void cambiarTipoListado(String tipo) {
+		TIPO_TRADUCCION = tipo;
+		notifyDataSetChanged();
+	}
+
+	public List<Palabra> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Palabra> lista) {
+		this.lista = lista;
+	}
+
+	@Override
+	public int getItemCount() {
+		return lista.size();
+	}
+
+	@Override
+	public long getItemId(int id) {
+		return id;
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		return position;
+	}
+}
+
---
+++ SayTheWord/SayTheWord/app/src/main/java/com/example/saytheword/Services/RealmService.java
@@ -0,0 +1,138 @@
+package com.example.saytheword.Services;
+
+import android.content.Context;
+
+import com.example.saytheword.Models.Palabra;
+import com.example.saytheword.Models.VerboIrregular;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+public class RealmService {
+
+	public static AtomicInteger palabraId = new AtomicInteger();
+	public static AtomicInteger verboIrregularId = new AtomicInteger();
+
+	public void setearConfiguracion(Context context) {
+
+		// ----- Setear configuracion
+		Realm.init(context);
+		RealmConfiguration config = new RealmConfiguration.Builder().build();
+		Realm.setDefaultConfiguration(config);
+		//-----------------
+
+		Realm realm = Realm.getDefaultInstance();
+		palabraId = getIdByTabla(realm, Palabra.class);
+		verboIrregularId = getIdByTabla(realm, VerboIrregular.class);
+		realm.close();
+	}
+
+	public <T extends RealmObject> AtomicInteger getIdByTabla(Realm realm, Class<T> clase) {
+		RealmResults<T> results = realm.where(clase).findAll();
+
+		return (results.size() > 0) ? new AtomicInteger(results.max("id").intValue()) : new AtomicInteger();
+	}
+
+	// -------------------------- Palabra
+	public List<Palabra> getPalabras() {
+		return Realm.getDefaultInstance().where(Palabra.class).findAll();
+	}
+
+	public List<Palabra> getPalabrasProblematicas() {
+		return Realm.getDefaultInstance().where(Palabra.class).equalTo("palabraProblematica", true).findAll();
+	}
+
+	public void insertarPalabras(final List<Palabra> palabras) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				Realm.getDefaultInstance().insert(palabras);
+			}
+		});
+	}
+
+	public void cambiarMostrarRespuestasPalabras(final List<Palabra> lista, final boolean valor) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				for (Palabra x : lista) {
+					x.setMostrarRespuesta(valor);
+				}
+			}
+		});
+	}
+
+	public void cambiarMostrarRespuestaPalabra(final Palabra x) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				x.setMostrarRespuesta(!x.isMostrarRespuesta());
+			}
+		});
+	}
+
+	public void cambiarPalabraProblematicaPalabra(final Palabra x) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				x.setPalabraProblematica(!x.isPalabraProblematica());
+			}
+		});
+	}
+
+	// ----------------------------------------------------
+	// -------------------------- Verbo Irregular
+	public List<VerboIrregular> getIrregularVerbs() {
+		return Realm.getDefaultInstance().where(VerboIrregular.class).findAll();
+	}
+
+	public List<VerboIrregular> getIrregularVerbsProblematicos() {
+		return Realm.getDefaultInstance().where(VerboIrregular.class).equalTo("palabraProblematica", true).findAll();
+	}
+
+	public void insertarIrregularVerbs(final List<VerboIrregular> palabras) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				Realm.getDefaultInstance().insert(palabras);
+			}
+		});
+	}
+
+	public void cambiarMostrarRespuestasVerbosIrregulares(final List<VerboIrregular> lista, final boolean valor) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				for (VerboIrregular x : lista) {
+					x.setMostrarRespuesta(valor);
+				}
+			}
+		});
+	}
+
+	public void cambiarMostrarRespuestaVerbosIrregulares(final VerboIrregular x) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				x.setMostrarRespuesta(!x.isMostrarRespuesta());
+			}
+		});
+	}
+
+	public void cambiarPalabraProblematicaVerbosIrregulares(final VerboIrregular x) {
+		Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+			@Override
+			public void execute(Realm realm) {
+				x.setPalabraProblematica(!x.isPalabraProblematica());
+			}
+		});
+	}
+	// ----------------------------------------------------
+
+
+}
