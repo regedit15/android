00b8902ca0728dfb282f9598900e0fbbb309b1ae
Martin <martinrossi9009@gmail.com>
2021-12-20 21:11:17 -0300

    Se crea proyecto de EditGoogleSheets

---
+++ EditGoogleSheets/app/src/main/java/martin/editgooglesheets/MainActivity.java
@@ -0,0 +1,31 @@
+package martin.editgooglesheets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static String google_api_key = "your_api_key";
+
+    // PruebaMartin
+    public static String spreadsheet_id = "1E1z-MY5q3jbX0dIX1W8KHxT7mpbwpMeWKYxutdpHd6E";
+
+    // https://docs.google.com/spreadsheets/d/e/2PACX-1vRzKshTik1k7aFItB-HJTKHxrCWzvOBP2q1B3Deh-owUG69oHuiponqeP4bwMWTu-58zRSEYj0sUwFJ/pubhtml
+
+
+    // https://docs.google.com/spreadsheets/d/1E1z-MY5q3jbX0dIX1W8KHxT7mpbwpMeWKYxutdpHd6E/gviz/tq
+
+    // http://script.ccocle.com/macros/s/AXfvcbx0LElu'Ocvl-ZUeriKaEvKI6akTLUaYftAc7NCM_IRTL3ESuDk/exec?id-YCUR_SPREAD_SHEET_ID&sheet=Sheetl
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
