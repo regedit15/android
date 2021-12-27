52a8494fa91b4517b439ab61e077f0a34916280b
Martin <martinrossi9009@gmail.com>
2021-12-27 18:57:25 -0300

    se ordena proyecto

--- Google Sheets Api/EditGoogleSheets_v2/app/src/main/java/martin/googlesheetsapi/MainActivity.java
+++ Google Sheets Api/EditGoogleSheets_v2/app/src/main/java/martin/googlesheetsapi/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //-----------------------------------------------------
+        //------------------------------------------------------
 
         try {
             sheetsService = SheetsQuickstart222.getSheetsService(this);
