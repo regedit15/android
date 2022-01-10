81a796301d6d28a251750f043cc43f198a9b5777
Martin <martinrossi9009@gmail.com>
2022-01-10 13:37:59 -0300

    se continua

--- Google Sheets Api/EditGoogleSheets_V6/app/src/main/java/martin/googlesheetsapi/MainActivity.java
+++ Google Sheets Api/EditGoogleSheets_V6/app/src/main/java/martin/googlesheetsapi/MainActivity.java
@@ -4,14 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
-
-import martin.googlesheetsapi.services.GoogleSheetsApiService;
+import martin.googlesheetsapi.services.UtilService;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String SHEET_1 = "Hoja1!";
+    private UtilService utilService = new UtilService(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        GoogleSheetsApiService googleSheetsApiService = new GoogleSheetsApiService(this);
+        // GoogleSheetsApiService googleSheetsApiService = new GoogleSheetsApiService(this);
+        //
+        // googleSheetsApiService.getDatos(SHEET_1 + "A1:C2", values -> {
+        //     if (values == null || values.isEmpty()) {
+        //         System.out.println("No data found .");
+        //     } else {
+        //         for (List row : values) {
+        //             System.out.printf("%s %s from %s\n", row.get(5), row.get(4), row.get(1));
+        //         }
+        //     }
+        // });
 
-        googleSheetsApiService.getDatos(SHEET_1 + "A1:C2", values -> {
-            if (values == null || values.isEmpty()) {
-                System.out.println("No data found .");
-            } else {
-                for (List row : values) {
-                    System.out.printf("%s %s from %s\n", row.get(5), row.get(4), row.get(1));
-                }
-            }
-        });
+
+        utilService.ceateTable(
+                R.id.table_main
+                , new String[]{"Titulo Col 1", "Titulo Col 2", "Titulo Col 3", "Titulo Col 4"}
+                , new String[]{"a1", "a2", "a3", "a4"}
+                , new String[]{"b1", "b2", "b3", "b4"}
+        );
+
+
     }
 
 
---
+++ Google Sheets Api/EditGoogleSheets_V6/app/src/main/java/martin/googlesheetsapi/services/UtilService.java
@@ -0,0 +1,49 @@
+package martin.googlesheetsapi.services;
+
+import android.graphics.Color;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import martin.googlesheetsapi.MainActivity;
+
+public class UtilService {
+
+    private MainActivity main;
+
+    public UtilService(MainActivity main) {
+        this.main = main;
+    }
+
+    public void addRow(Integer idTableLayout, String... titulos) {
+        TableLayout tableLayout = main.findViewById(idTableLayout);
+        TableRow tbrow0 = new TableRow(main);
+        TextView textView;
+
+        for (String titulo : titulos) {
+            textView = new TextView(main);
+            textView.setText(titulo);
+            textView.setTextColor(Color.WHITE);
+            tbrow0.addView(textView);
+        }
+
+        tableLayout.addView(tbrow0);
+    }
+
+    public void ceateTable(Integer idTableLayout, String[]... rows) {
+        TableLayout tableLayout = main.findViewById(idTableLayout);
+        TableRow tableRow;
+        TextView textView;
+        for (String[] row : rows) {
+            tableRow = new TableRow(main);
+
+            for (String cell : row) {
+                textView = new TextView(main);
+                textView.setText(cell);
+                textView.setTextColor(Color.WHITE);
+                tableRow.addView(textView);
+            }
+            tableLayout.addView(tableRow);
+        }
+    }
+}
