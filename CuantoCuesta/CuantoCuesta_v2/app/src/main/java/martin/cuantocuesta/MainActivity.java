dd3647182133c6bb44744b5f032ea94119eca8e8
Martin <martinrossi9009@gmail.com>
2024-02-12 13:31:01 -0300

    cuanto cuesta v2

---
+++ CuantoCuesta/CuantoCuesta_v2/app/src/main/java/martin/cuantocuesta/MainActivity.java
@@ -0,0 +1,14 @@
+package martin.cuantocuesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
