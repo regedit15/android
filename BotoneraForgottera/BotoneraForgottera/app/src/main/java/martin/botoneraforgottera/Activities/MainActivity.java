13b6d4737c46a75b43c85b820590ce4764c5f1b6
Martin <martinrossi9009@gmail.com>
2024-02-25 19:38:54 -0300

    bottonre forgottera update

--- BotoneraForgottera/BotoneraForgottera/app/src/main/java/martin/botoneraforgottera/Activities/MainActivity.java
+++ BotoneraForgottera/BotoneraForgottera/app/src/main/java/martin/botoneraforgottera/Activities/MainActivity.java
@@ -30,134 +30,128 @@ import martin.botoneraforgottera.Sticker.StickerPack;
 
 public class MainActivity extends AppCompatActivity implements StickerListener {
 
-    //Nota sobre los stickers: cada stickers debe estar en formato .webp y debe tener 512x512 pixeles
-    // y pesar menos de 100 kb
-    // La imagen del icono del paquete debe tener 96x96 pixeles y pesar menos de 50 kb
-    //   sasas 22323
+	// Nota sobre los stickers: cada stickers debe estar en formato .webp y debe tener 512x512 pixeles
+	// y pesar menos de 100 kb
+	// La imagen del icono del paquete debe tener 96x96 pixeles y pesar menos de 50 kb
+	//   sasas 22323
 
-    private final String DATOS_GUARDADOS = "DATOS_GUARDADOS";
-    private final String APP_VERSION = "VERSION";
-    private final int PERMISO_WRITE_OK = 1;
-    private DrawerLayout drawerLayout;
-    public NavigationView navigationView;
-    public RealmService realmService = new RealmService();
-    public UtilService utilService = new UtilService();
+	private final String DATOS_GUARDADOS = "DATOS_GUARDADOS";
+	private final String APP_VERSION = "VERSION";
+	private final int PERMISO_WRITE_OK = 1;
+	private DrawerLayout drawerLayout;
+	public NavigationView navigationView;
+	public RealmService realmService = new RealmService();
+	public UtilService utilService = new UtilService();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+		Toolbar myToolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(myToolbar);
 
-        // Habilitar un icono en el toolbar
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                boolean transaction = false;
-                Fragment fragment = null;
-
-                switch (menuItem.getItemId()) {
-                    case R.id.it_audios:
-                        fragment = new AudiosFragment(AudiosFragment.TIPO_DEFAULT);
-                        transaction = true;
-                        break;
-                    case R.id.it_favoritos:
-                        fragment = new AudiosFragment(AudiosFragment.TIPO_FAVORITO);
-                        transaction = true;
-                        break;
-                    case R.id.it_gif:
-                        fragment = new GifFragment();
-                        transaction = true;
-                        break;
-                    case R.id.it_stickers:
-                        fragment = new StickersCargandoFragment();
-                        transaction = true;
-                        break;
-
-                }
-
-                if (transaction) {
-                    cambiarFragment(fragment, menuItem);
-                    // Escondemos el menu lateral
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                return true;
-            }
-        });
-
-        cambiarFragment(new AudiosFragment(AudiosFragment.TIPO_DEFAULT), navigationView.getMenu().getItem(0));
-
-        //	 ---------------------- Se piden permisos
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // pregunta al usuario los permisos
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISO_WRITE_OK);
-        }
-        // ------------------------------------------------
-
-        realmService.setearConfiguracion(getApplicationContext());
+		// Habilitar un icono en el toolbar
+		getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        //	 ---------------------- Nos fijamos si tiene los audios gardados y actualizados, sino los insertamos
-        utilService.setsharedPreferenceService(this);
-        if (!utilService.getPreferenceService().getBoolean(DATOS_GUARDADOS)) {
-            realmService.eliminarTodo();
-            realmService.insertarAudios(utilService.getAudios());
-            utilService.getPreferenceService().guardarBooleanYCommitear(DATOS_GUARDADOS, true);
-        }
+		drawerLayout = findViewById(R.id.drawer_layout);
+		navigationView = findViewById(R.id.navigation_view);
 
-        String appVersion = utilService.getVersionName(this);
-        if (!appVersion.equals(utilService.getPreferenceService().getString(APP_VERSION)) || (utilService.getAudios().size() != realmService.getAudios().size())) {
-            utilService.getPreferenceService().guardarStringYCommitear(APP_VERSION, appVersion);
-            utilService.insertarDatosSinModificarFavoritos();
-        }
-        // ------------------------------------------------
-    }
+		navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+				boolean transaction = false;
+				Fragment fragment = null;
 
-    private void cambiarFragment(Fragment fragment, MenuItem menuItem) {
-        //cambiamos de fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+				if (menuItem.getItemId() == R.id.it_audios) {
+					fragment = new AudiosFragment(AudiosFragment.TIPO_DEFAULT);
+					transaction = true;
+				} else if (menuItem.getItemId() == R.id.it_favoritos) {
+					fragment = new AudiosFragment(AudiosFragment.TIPO_FAVORITO);
+					transaction = true;
+				} else if (menuItem.getItemId() == R.id.it_gif) {
+					fragment = new GifFragment();
+					transaction = true;
+				} else if (menuItem.getItemId() == R.id.it_stickers) {
+					fragment = new StickersCargandoFragment();
+					transaction = true;
+				}
 
-        //para que se muestre seleccionado
-        menuItem.setChecked(true);
+				if (transaction) {
+					cambiarFragment(fragment, menuItem);
+					// Escondemos el menu lateral
+					drawerLayout.closeDrawer(GravityCompat.START);
+				}
+				return true;
+			}
+		});
 
-        // Seteamos que el titulo de la barra sea igual que el item
-        getSupportActionBar().setTitle(menuItem.getTitle());
-    }
+		cambiarFragment(new AudiosFragment(AudiosFragment.TIPO_DEFAULT), navigationView.getMenu().getItem(0));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        boolean resultado;
+		//	 ---------------------- Se piden permisos
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			// pregunta al usuario los permisos
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISO_WRITE_OK);
+		}
+		// ------------------------------------------------
 
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                resultado = true;
-                drawerLayout.openDrawer(GravityCompat.START);
-                break;
-            default:
-                resultado = super.onOptionsItemSelected(item);
-                break;
-        }
-        return resultado;
-    }
+		realmService.setearConfiguracion(getApplicationContext());
 
-    @Override
-    public void listarStickers(StickerPack stickerPack) {
-        Fragment fragment = new StickersListadoFragment(stickerPack);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
-    }
 
-    @Override
-    public void listarPaquetes(ArrayList<StickerPack> stickerPackList) {
-        Fragment fragment = new StickersListadoPaquetesFragment(stickerPackList);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
-    }
+		//	 ---------------------- Nos fijamos si tiene los audios gardados y actualizados, sino los insertamos
+		utilService.setsharedPreferenceService(this);
+		if (!utilService.getPreferenceService().getBoolean(DATOS_GUARDADOS)) {
+			realmService.eliminarTodo();
+			realmService.insertarAudios(utilService.getAudios());
+			utilService.getPreferenceService().guardarBooleanYCommitear(DATOS_GUARDADOS, true);
+		}
+
+		String appVersion = utilService.getVersionName(this);
+		if (!appVersion.equals(utilService.getPreferenceService().getString(APP_VERSION)) || (utilService.getAudios().size() != realmService.getAudios().size())) {
+			utilService.getPreferenceService().guardarStringYCommitear(APP_VERSION, appVersion);
+			utilService.insertarDatosSinModificarFavoritos();
+		}
+		// ------------------------------------------------
+	}
+
+	private void cambiarFragment(Fragment fragment, MenuItem menuItem) {
+		// cambiamos de fragment
+		getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+
+		// para que se muestre seleccionado
+		menuItem.setChecked(true);
+
+		// Seteamos que el titulo de la barra sea igual que el item
+		getSupportActionBar().setTitle(menuItem.getTitle());
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean resultado;
+
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				resultado = true;
+				drawerLayout.openDrawer(GravityCompat.START);
+				break;
+			default:
+				resultado = super.onOptionsItemSelected(item);
+				break;
+		}
+		return resultado;
+	}
+
+	@Override
+	public void listarStickers(StickerPack stickerPack) {
+		Fragment fragment = new StickersListadoFragment(stickerPack);
+		getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+	}
+
+	@Override
+	public void listarPaquetes(ArrayList<StickerPack> stickerPackList) {
+		Fragment fragment = new StickersListadoPaquetesFragment(stickerPackList);
+		getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+	}
 }
