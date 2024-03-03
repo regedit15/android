f0c928b6e10fedc45ed49b025434d34548ccca58
Martin <martinrossi9009@gmail.com>
2024-03-03 18:20:22 -0300

    se crea app de ejemplo de solicitud de permisos

--- Solicitar Permisos/SolicitarPermisos/app/src/main/java/martin/solicitarpermisos/PermissionService.java
+++ Solicitar Permisos/SolicitarPermisos/app/src/main/java/martin/solicitarpermisos/PermissionService.java
@@ -1,65 +1,56 @@
 package martin.solicitarpermisos;
 
-
 import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermissionService {
 
-	private int currentIndex = 0;
-	private int currentIndex2 = 0;
+	// private int currentIndex = 0;
+	// private int currentIndex2 = 0;
 	private static final int PERMISSION_REQ_CODE = 100;
 
-	private void showNextDialog(Activity activity, String[] permissions) {
-		new AlertDialog.Builder(activity)
-				.setMessage("Esta aplicación necesita el permiso de " + permissions[currentIndex].split("\\.")[2] + " para continuar")
-				.setTitle("Permiso requerido")
-				.setCancelable(false)
-				.setPositiveButton("ok", (dialog, which) -> {
+	private int allQuantityPermissionsOk = 0;
 
-					ActivityCompat.requestPermissions(activity, new String[]{permissions[currentIndex]}, PERMISSION_REQ_CODE);
-					dialog.dismiss();
-
-					currentIndex++;
-					showNextDialog(activity, permissions);
-				})
-				.setNegativeButton("Cancelar", (dialog, which) -> {
-					dialog.dismiss();
-				})
-				.show();
-	}
+	// private void showNextDialog(Activity activity, String[] permissions) {
+	// 	new AlertDialog.Builder(activity)
+	// 			.setMessage("Esta aplicación necesita el permiso de " + permissions[currentIndex].split("\\.")[2] + " para continuar")
+	// 			.setTitle("Permiso requerido")
+	// 			.setCancelable(false)
+	// 			.setPositiveButton("ok", (dialog, which) -> {
+	//
+	// 				ActivityCompat.requestPermissions(activity, new String[]{permissions[currentIndex]}, PERMISSION_REQ_CODE);
+	// 				dialog.dismiss();
+	//
+	// 				currentIndex++;
+	// 				showNextDialog(activity, permissions);
+	// 			})
+	// 			.setNegativeButton("Cancelar", (dialog, which) -> {
+	// 				dialog.dismiss();
+	// 			})
+	// 			.show();
+	// }
 
 	public void requestRuntimePermissions(Activity activity, String[] permissions) {
 
 		int quantityPermissionsOk = 0;
+		List<String> quantityPermissionsNegative = new ArrayList<String>();
+
 		for (String permission : permissions) {
 
 			if (ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED) {
 				quantityPermissionsOk++;
 			} else if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
-				showNextDialog(activity, permissions);
-				// new AlertDialog.Builder(activity)
-				// 		.setMessage("Esta aplicación necesita el permiso de " + permission.split("\\.")[2] + " para continuar")
-				// 		.setTitle("Permiso requerido")
-				// 		.setCancelable(false)
-				// 		.setPositiveButton("ok", (dialog, which) -> {
-				//
-				// 			ActivityCompat.requestPermissions(activity, new String[]{permission}, PERMISSION_REQ_CODE);
-				// 			dialog.dismiss();
-				// 		})
-				// 		.setNegativeButton("Cancelar", (dialog, which) -> {
-				// 			dialog.dismiss();
-				// 		})
-				// 		.show();
+
+				quantityPermissionsNegative.add(permission);
 			} else {
-				ActivityCompat.requestPermissions(activity, new String[]{permission}, PERMISSION_REQ_CODE);
+				quantityPermissionsNegative.add(permission);
 			}
 		}
 
@@ -70,30 +61,57 @@ public class PermissionService {
 			} else {
 				message = "Todos los permisos han sido concedidos, puede usar la app!";
 			}
-			Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+
+			Toast.makeText(activity, message.toString(), Toast.LENGTH_SHORT).show();
+		} else {
+			StringBuilder message = new StringBuilder("La aplicación necesita los siguientes permisos para continuar: \n\n");
+
+
+			for (String p : quantityPermissionsNegative) {
+				message.append("\n- ").append(p.split("\\.")[2]);
+			}
+
+			new AlertDialog.Builder(activity)
+					.setMessage(message)
+					.setTitle("Permisos requeridos")
+					.setCancelable(false)
+					.setPositiveButton("ok", (dialog, which) -> {
+
+
+						for (String p : quantityPermissionsNegative) {
+							ActivityCompat.requestPermissions(activity, new String[]{p}, PERMISSION_REQ_CODE);
+						}
+						dialog.dismiss();
+
+					})
+					.setNegativeButton("Cancelar", (dialog, which) -> {
+						dialog.dismiss();
+					})
+					.show();
+
 		}
 	}
 
 
-	private void showNextDialog2(Activity activity, String[] permissions, String packageName) {
-
-		new AlertDialog.Builder(activity)
-				.setMessage("Esta aplicación necesita el permiso de " + permissions[currentIndex2].split("\\.")[2] + " que has rechazado, por favor conceda el permiso llendo a los ajustes")
-				.setCancelable(false)
-				.setPositiveButton("Ir a Settings", (dialog, which) -> {
-
-					activity.startActivity(
-							new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-									.setData(Uri.fromParts("package", packageName, null))
-					);
-					dialog.dismiss();
-					currentIndex2++;
-					showNextDialog2(activity, permissions, packageName);
-				})
-				.setNegativeButton("Cancelar", (dialog, which) -> {
-					dialog.dismiss();
-				}).show();
-	}
+	// private void showNextDialog2(Activity activity, String[] permissions, String packageName) {
+	//
+	// 	new AlertDialog.Builder(activity)
+	// 			.setMessage("Esta aplicación necesita el permiso de " + permissions[currentIndex2].split("\\.")[2] + " que has rechazado, por favor conceda el permiso llendo a los ajustes")
+	// 			.setCancelable(false)
+	// 			.setPositiveButton("Ir a Settings", (dialog, which) -> {
+	//
+	// 				activity.startActivity(
+	// 						new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+	// 								.setData(Uri.fromParts("package", packageName, null))
+	// 				);
+	// 				dialog.dismiss();
+	// 				currentIndex2++;
+	// 				showNextDialog2(activity, permissions, packageName);
+	// 			})
+	// 			.setNegativeButton("Cancelar", (dialog, which) -> {
+	// 				dialog.dismiss();
+	// 			}).show();
+	// }
 
 	public void onRequestPermissionsResult(Activity activity, int requestCode, int[] grantResults, String packageName, String[] permissions) {
 
@@ -102,11 +120,13 @@ public class PermissionService {
 			if (requestCode == PERMISSION_REQ_CODE) {
 				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-					Toast.makeText(activity, "Permiso concedido, puede usar la app!", Toast.LENGTH_SHORT).show();
+					// Toast.makeText(activity, "Permiso concedido, puede usar la app!", Toast.LENGTH_SHORT).show();
+
+					allQuantityPermissionsOk++;
 
 				} else if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
 
-					showNextDialog2(activity, permissions, packageName);
+					// showNextDialog2(activity, permissions, packageName);
 					// new AlertDialog.Builder(activity)
 					// 		.setMessage("Esta aplicación necesita el permiso de " + permission.split("\\.")[2] + " que has rechazado, por favor conceda el permiso llendo a los ajustes")
 					// 		.setCancelable(false)
